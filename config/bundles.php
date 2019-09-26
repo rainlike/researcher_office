1ec6fad039b48e23d315e65857c047d4dbfefc78
Alex Saveliev <savelievaleks@hotmail.com>
2019-09-27 00:56:09 +0300

    test

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['prod' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
